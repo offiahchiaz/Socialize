68d3d56b5e535ca60aaad5f5ce21b63259010b62
offiahchiaz <offiahchiaz@gmail.com>
2020-06-28 19:24:38 +0100

    Added userblock partial

---
+++ resources/views/user/partials/userblock.blade.php
@@ -0,0 +1,10 @@
+<div class="media">
+    <a href="" class="pull-left">
+        <img src="" alt="" class="media-object">
+    </a>
+    <div class="media-body">
+        <h4 class="media-heading">
+            <a href="">Okeke</a>
+        </h4>
+    </div>
+</div>

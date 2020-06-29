b18918f3fa52e0c945f04c812fb97d0f064a063b
offiahchiaz <offiahchiaz@gmail.com>
2020-06-29 15:30:18 +0000

    Added isFriendsWith method

--- app/User.php
+++ app/User.php
@@ -42,47 +42,56 @@ class User extends Authenticatable
         return "https://www.gravatar.com/avatar/{{ md5($this->email) }}?d=mm&s=40";
     }
 
+    // Someone user_id sent a friend request to.
     public function friendOfMine()
     {
         return $this->belongsToMany(User::class, 'friends', 'user_id', 'friend_id');
     }
 
+    // Someone that recieved my friend request.
     public function friendOf()
     {
         return $this->belongsToMany(User::class, 'friends', 'friend_id', 'user_id');
     }
 
+    // Friend request that has been accepted.
     public function friends()
     {
         return $this->friendOfMine()->wherePivot('accepted', true)->get()
             ->merge($this->friendOf()->wherePivot('accepted', true)->get());
     }
 
+    // Friend request sent but yet to be accepted.
     public function friendRequests()
     {
         return $this->friendOfMine()->wherePivot('accepted', false)->get();
     }
 
+    // Friend request recieved but yet to be accepted
     public function friendRequestPending()
     {
         return $this->friendOf()->wherePivot('accepted', false)->get();
     }
 
+    // Check if user has a friend request pending from another user
     public function hasFriendRequestPending(User $user)
     {
         return (bool) $this->friendRequestPending()->where('id', $user->id)->count();
     }
 
+    // Check if we have received a friend request from a particular user
     public function hasFriendRequestRecieved(User $user)
     {
         return (bool) $this->friendRequests()->where('id', $user->id)-count();
     }
 
+    // Method to add a friend
     public function addFriend(User $user)
     {
         $this->friendOf()->attach($user->id);
     }
 
+    // This method accepts a friend request
     public function acceptFriendRequest(User $user)
     {
         $this->friendRequests()->where('id', $user->id)->first()
@@ -90,5 +99,11 @@ class User extends Authenticatable
                 'accepted' => true
             ]);
         // $this->friendRequests()->where('id', $user->id)->first()->pivot->update(['accepted', => true]);
+    }
+
+    // Tells if we are friends with a particular user
+    public function isFriendsWith(User $user)
+    {
+        return (bool) $this->friends()->where('id', $user->id)->count();
     }
 }

a1bb4de5f7268984630c11004ef474232d22fa20
offiahchiaz <offiahchiaz@gmail.com>
2020-06-29 06:04:47 +0100

    Added several friends requests methods

--- app/User.php
+++ app/User.php
@@ -62,4 +62,33 @@ class User extends Authenticatable
     {
         return $this->friendOfMine()->wherePivot('accepted', false)->get();
     }
+
+    public function friendRequestPending()
+    {
+        return $this->friendOf()->wherePivot('accepted', false)->get();
+    }
+
+    public function hasFriendRequestPending(User $user)
+    {
+        return (bool) $this->friendRequestPending()->where('id', $user->id)->count();
+    }
+
+    public function hasFriendRequestRecieved(User $user)
+    {
+        return (bool) $this->friendRequests()->where('id', $user->id)-count();
+    }
+
+    public function addFriend(User $user)
+    {
+        $this->friendOf()->attach($user->id);
+    }
+
+    public function acceptFriendRequest(User $user)
+    {
+        $this->friendRequests()->where('id', $user->id)->first()
+            ->pivot->update([
+                'accepted' => true
+            ]);
+        // $this->friendRequests()->where('id', $user->id)->first()->pivot->update(['accepted', => true]);
+    }
 }
